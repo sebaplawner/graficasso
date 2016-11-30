459dbfdab02eac9e548705dab8b21e5bbd97373a
sebaplawner <sebastian.plawner@gmail.com>
2016-11-30 16:31:17 -0300

    JS ajustes de camara y correccion plugin

--- www/js/index.js
+++ www/js/index.js
@@ -32,7 +32,9 @@ var app = {
 var workspace = {
     initialize: function() {
         text_displayed = false;
-        newGrid(32);
+        this.newGrid(32);
+        elements_ongrid = [];
+        element_id = null;
 
         var $panzoom = $('.workspace-grid').panzoom({
             minScale: .5,
@@ -61,20 +63,72 @@ var workspace = {
         $('#text-button').on('click', function(e) {
             if(text_displayed) {
                 $('.left-panel').animate({'left': -$('.left-panel').outerWidth()+40+'px'}, 'slow');
-                $('.bottom-panel').animate({'left': '-='+$('.bottom-panel').outerWidth()/2+'px'}, 'slow')                
+                $('.bottom-panel').animate({'left': $(window).width()/2 - $('.bottom-panel').outerWidth()/2+'px'}, 'slow')
             } else {
                 $('.left-panel').animate({'left': '0'}, 'slow');
-                $('.bottom-panel').animate({'left': '+='+$('.bottom-panel').outerWidth()/2+'px'}, 'slow')
+                $('.bottom-panel').animate({'left': $(window).width()*.75 - $('.bottom-panel').outerWidth()/2+'px'}, 'slow')
             }
             
             text_displayed = !text_displayed;
         });
+
+        $('#camera-button').on('click', function(e) {
+            $('.workspace-grid').css({'-webkit-transform': 'translate(0px, 0px)'});
+        });
+
+        $('.bottom-panel > img').on('click', function(e) {
+            $('.bottom-panel').animate({'bottom': -$('.bottom-panel').outerHeight()+'px'}, 'slow');
+            $('#cancel-button').show();
+            element_id = $(this).attr('id');
+            console.log('Item selected: ' + element_id);
+
+            if(text_displayed) {
+                $('#text-button').click();
+            }
+        });
+
+        $('.square').on('click', function(e) {
+            if(element_id !== null) {
+                var row = $(this).data('row');
+                var col = $(this).data('col');
+
+                console.log('Square selected: ['+row+','+col+']');
+            }
+        });
+    },
+
+    toggleFinishButton: function(value) {
+        if(value) {
+            $('#finish-button').show();
+            $('#camera-button').css('right', '80px');
+        } else {
+            $('#finish-button').hide();
+            $('#camera-button').css('right', '0px');
+        }
+    },
+
+    newGrid: function(n) {
+        $(".square").remove();
+
+        var distance = 0;
+        var jumpline = 0;
+        for (var k = 0; k < n; k++) {
+            for (var i = 0; i < n; i++) {
+                $(".workspace-grid").append($("<div></div>").data('row', i).data('col', k).addClass("square").height(56).width(128).css({'marginLeft': distance + 'px', 'marginTop': (jumpline * 58) + 'px'}));
+                distance += 128;
+            }
+
+            distance = 0;
+            jumpline++;
+        }
     }
 };
 
 app.initialize();
 
 function onGridPan(e, panzoom) {
+    e.preventDefault();
+    /*
     var transform = $('.workspace-grid').css('transform').slice(7, -1).split(',');
 
     var maxX = 683,
@@ -84,9 +138,9 @@ function onGridPan(e, panzoom) {
 
     if(parseInt(transform[0]) < 1) {
         maxX = 542;
-        minX = -945;
+        minX = -892;
         maxY = 604;
-        minY = -8;
+        minY = 19;
     } else if(parseInt(transform[0]) > 1) {
         maxX = 968;
         minX = -6667;
@@ -105,20 +159,5 @@ function onGridPan(e, panzoom) {
         transform[5] = minY
 
     $('.workspace-grid').css('transform', 'matrix('+transform[0]+', 0, 0, '+transform[3]+', '+transform[4]+', '+transform[5]+')');
-}
-
-function newGrid(n) {
-    $(".square").remove();
-
-    var distance = 0;
-    var jumpline = 0;
-    for (var k = 0; k < n; k++) {
-        for (var i = 0; i < n; i++) {
-            $(".workspace-grid").append($("<div></div>").addClass("square").height(56).width(128).css({'marginLeft': distance + 'px', 'marginTop': (jumpline * 58) + 'px'}));
-            distance += 128;
-        }
-
-        distance = 0;
-        jumpline++;
-    }
+    */
 }
